1b4ba81db7546549ca222e13fd497f1f6acb3c10
Cristian Morales <cr15moresp@gmail.com>
2021-04-19 16:01:32 -0500

    se creó una promesa en la función buscar héroe

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
-import { buscarHeroe } from './js/callbacks'
+import { buscarHeroe as buscarHeroeCallback } from './js/callbacks' // Se asigna un alias al metodo en callbacks para que el
+                                                                    // intérprete no lo confunda con el de promesas
+import { buscarHeroe } from './js/promesas'
 import './styles.css';
 
 const heroeId = 'capi';
+const heroe2Id = 'iron';
+const heroe3Id = 'spider';
 
-buscarHeroe( heroeId,  ( err, heroe ) => {
-    if ( err )
-        console.error( err );
-    else
-        console.log( heroe );
+
+// Cuando se empiezan a necesitar más llamas a un callback y se empieza a hacer un multiple anidamiento se denomina
+// CALLBACK HELL lo cual a futuro es un 'dolor de cabeza', apaprtir de este problema nacieron las PROMESAS
+
+// buscarHeroe( heroeId,  ( err, heroe ) => {
+//     if ( err ) return console.error( err );
+    
+//     buscarHeroe( heroe2Id, (err, heroe2) => {   // Se uso el nombre heroe2 para evitar problemas con el interprete
+//                                                 // ya que no se sabe si se remplazaria con el heroe ya existente
+//         if ( err ) return console.error( err );
+
+//         buscarHeroe( heroe3Id, (err, heroe3) => {   // Se uso el nombre heroe2 para evitar problemas con el interprete
+//                                                     // ya que no se sabe si se remplazaria con el heroe ya existente
+//             if ( err ) return console.error( err );
+//             console.log(`Enviando a ${heroe.nombre}, ${heroe2.nombre} y ${heroe3.nombre} a la mision`);
+//         });
+
+//     });
+// });
+
+buscarHeroe( heroeId ).then( heroe => {     // Esto se ejecutará despues de el log final debido a que pasa a otro hilo de ejecucion
+                                            // mientras el programa sigue corriendo la promesa realiza su procedimiento para
+                                            // porsteriormente devolver la respuesta
+    console.log(`Enviando a ${heroe.nombre} a la mision`);
 });
+
+console.log('Fin del programa');
---
+++ src/js/promesas.js
@@ -0,0 +1,32 @@
+const heroes = {
+    capi: {
+        nombre: 'Capitán América',
+        poder:  'Aguantar inyeccciones sin morir'
+    },
+    iron: {
+        nombre: 'Ironman',
+        poder:  'Absurda cantidada de dinero'
+    },
+    spider: {
+        nombre: 'Spiderman',
+        poder:  'La mejor reacción alérgica a las picaduras de araña'
+    },
+}
+
+
+export const buscarHeroe = ( id ) => {      
+
+    const heroe = heroes[id];
+
+    return new Promise( ( resolve, reject ) => {    // Esta es la estructura de una promesa, el resolve y reject pueden usarse si es
+                                                    // que se encesitan en la promesa, sino solamente no se reciben com oargumentos
+        if ( heroes ){
+            resolve( heroe );    // Aquí se colocan los argumentos que se quieren regresar a la funcion que llama a la promesa
+        } else {
+            reject(`No existe un héroe con el id ${ id }`);
+        }
+
+    });
+
+}
+
