7d465279241fade833859fc1155338b67e7606cc
Cristian Morales <cr15moresp@gmail.com>
2021-04-19 18:23:31 -0500

    Se implementó el promise.race para ejecutar la promesa que termine primero

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+import { promesaLenta, promesaMedia, promesaRapida } from './js/promesas';
 
-import './styles.css';
+// promesaLenta.then( console.log );
+// promesaMedia.then( mensaje => console.log( mensaje ) ); // Lo mismo que la linea inferior y superior
+// promesaRapida.then( console.log );
 
+// Promise.race permite ejecutar varias promesas en conjunto y obtener el mensaje de la promesa que se resulve primero
+// para el race no importa si alguna falla, ejecutara la promesa que termine primero aun cuando tenga un error 
 
-
+Promise.race([ promesaLenta, promesaMedia, promesaRapida ])
+    .then( mensaje => { console.log(mensaje); })
+    .catch( console.warn );
